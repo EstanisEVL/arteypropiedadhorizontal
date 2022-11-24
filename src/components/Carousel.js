6434ca9a08ee4dcc881596e979e21e965a1cc5be
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-11-24 12:19:27 -0300

    Third commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,29 +1,27 @@
-import React from "react";
-import { Autoplay, Pagination, Navigation } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import CarouselSlide from "./CarouselSlide";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import CarouselSlide from './CarouselSlide';
 
 const Carousel = props => {
   return(
     <div className='carousel-container'>
       <Swiper
+        modules={[Pagination, Navigation, Autoplay]}
+        autoplay={true}
         slidesPerView={1}
-        spaceBetween={10}
+        spaceBetween={30}
         loop={true}
         autoHeight= {true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false
-        }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Pagination, Navigation, Autoplay]}
-        className="mySwiper"
+        className='mySwiper'
       >
         {props.images.map((item, index) => (
           <SwiperSlide key={index}>
