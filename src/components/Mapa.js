80ce00b53bc34a1f9177f5e988617893b9cdf024
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-11-25 12:45:57 -0300

    Fourth commit 1.0

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,16 +1,22 @@
+import useWindowSize from "../utils/hooks/useWindowSize";
 import BackToTopButton from "./BackToTopButton";
 import MapaAcordeonBtn from "./MapaAcordeonBtn";
 
 const Mapa = () => {
+  const size = useWindowSize();
+
   return(
-    <main className='map-section-container'>
+    <>
+      { size.width < 600 && <main className='map-section-container'>
       <h1 className='map-section-title'>Mapa de murales de Buenos Aires</h1>
       <div className='map-container'>
         <iframe title='mapa de murales' src="https://www.google.com/maps/d/embed?mid=145EW8-am_mRdY1Da8fYz6ufVejdxkYU&ehbc=2E312F" width="100%" height="480"></iframe>
       </div>
       <MapaAcordeonBtn />
       <BackToTopButton />
-    </main>
+    </main> }
+    </>
+    
   );
 };
 
